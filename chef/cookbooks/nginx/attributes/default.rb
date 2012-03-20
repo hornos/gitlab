ffd5c12a827f7b39aee0d70e4662963c4a7dac29
Edmund Haselwanter <edmund@haselwanter.com>
2012-03-20 11:51:39 +0100

    remove nginx

--- chef/cookbooks/nginx/attributes/default.rb
+++
@@ -1,9 +0,0 @@
-default[:nginx][:install_path] = "/usr/local/nginx"
-default[:nginx][:version]      = '1.0.9'
-default[:nginx][:url]          = "http://nginx.org/download/nginx-#{nginx[:version]}.tar.gz"
-default[:nginx][:conf_d]       = "#{nginx[:install_path]}/conf.d"
-
-default[:nginx][:nginx_name]       = "nginx"
-default[:nginx][:base_path]        = "/usr/local/nginx"
-default[:nginx][:log_path]        = "#{nginx[:base_path]}/logs"
-default[:nginx][:pid_path]         = "/var/run"
